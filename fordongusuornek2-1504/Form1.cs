a7727456b0a45fbbeda79e8c956ab878b7687ab4
akinseman <145451296+akinseman@users.noreply.github.com>
2023-10-23 13:30:39 +0300

    Add files via upload

---
+++ fordongusuornek2-1504/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fordongusuornek2_1504
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngoster_Click(object sender, EventArgs e)
+        {
+            for (int i = 25; i <= 75; i++)
+            {
+                lbsayılar.Items.Add(i);
+            }
+        }
+    }
+}
